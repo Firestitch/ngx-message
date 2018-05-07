5efe8cc4cb20f9ece02edf07389314cde5e88fbf
Raymond <ray@firestitch.com>
2018-05-07 11:29:01 -0400

    updated tools and removed demo and docs

--- demo/assets/components/banner-inline-example/banner-inline-example.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'banner-inline-example',
-  templateUrl: 'banner-inline-example.component.html',
-  styleUrls: [ 'banner-inline-example.component.css' ]
-})
-export class BannerInlineExampleComponent {
-}
