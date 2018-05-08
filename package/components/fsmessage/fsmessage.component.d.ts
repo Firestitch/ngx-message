2e475caa2eb930fe7e93cf38f4c42ab1bb992695
Raymond <ray@firestitch.com>
2018-05-08 14:21:07 -0400

    ignored package updated tools

--- package/components/fsmessage/fsmessage.component.d.ts
+++
@@ -1,9 +0,0 @@
-import { OnInit } from '@angular/core';
-export declare class FsMessageComponent implements OnInit {
-    fsType: string;
-    fsMessage: string;
-    private icons;
-    icon: string;
-    constructor();
-    ngOnInit(): void;
-}
--- package/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './fsmessage.module';
-export * from './fsmessage.service';
-export * from './components/fsmessagedialog/fsmessagedialog.component';
-export * from './components/fsmessages/fsmessages.component';
-export * from './components/fsmessage/fsmessage.component';
