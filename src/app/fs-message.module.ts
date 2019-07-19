9efb42d0ac126ae8f0a1d765ae63c3f2c8dbaaa2
Eugene Kubesh <e.kubesh@wis.eu.com>
2019-07-19 17:02:20 +0300

    fixed default config provider

--- src/app/fs-message.module.ts
+++ src/app/fs-message.module.ts
@@ -2,16 +2,15 @@ import { NgModule, ModuleWithProviders, CUSTOM_ELEMENTS_SCHEMA } from '@angular/
 import { CommonModule } from '@angular/common';
 import { MatDialogModule, MatIconModule, MatButtonModule } from '@angular/material';
 
+import { merge } from 'lodash-es';
+
 import { FsMessageDialogComponent } from './components/message-dialog/message-dialog.component';
 import { FsMessagesComponent } from './components/messages/messages.component';
 import { FsMessageComponent } from './components/message/message.component';
 import { FsMessage } from './message.service';
 import { FsMessageConfig } from './interfaces/message-config';
-import { FS_MESSAGE_CONFIG } from './injectors/message-config';
+import { FS_MESSAGE_CONFIG, FS_MESSAGE_DEFAULT_CONFIG } from './injectors/message-config';
 
-const FS_MESSAGE_DEFAULT_CONFIG = {
-  toastTimeout: 5
-}
 
 @NgModule({
   imports: [
@@ -41,17 +40,19 @@ export class FsMessageModule {
       ngModule: FsMessageModule,
       providers: [
         FsMessage,
+        { provide: FS_MESSAGE_DEFAULT_CONFIG, useValue: config },
         {
           provide: FS_MESSAGE_CONFIG,
-          useValue: Object.assign(
-            {},
-            FS_MESSAGE_DEFAULT_CONFIG,
-            config || {}
-          )
-        }
+          useFactory: FsMessageConfigFactory,
+          deps: [FS_MESSAGE_DEFAULT_CONFIG]
+        },
       ]
     };
   }
 }
 
+
+export function FsMessageConfigFactory(config: FsMessageConfig) {
+  return merge({ toastTimeout: 5 }, config);
+}
 
--- src/app/injectors/message-config.ts
+++ src/app/injectors/message-config.ts
@@ -1,3 +1,4 @@
 import { InjectionToken } from '@angular/core';
 
-export const FS_MESSAGE_CONFIG = new InjectionToken<any[]>('fs-message-config');
+export const FS_MESSAGE_CONFIG = new InjectionToken<any[]>('fs-message.config');
+export const FS_MESSAGE_DEFAULT_CONFIG = new InjectionToken<any[]>('fs-message.default-config');
