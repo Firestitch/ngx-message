5d59fb8bfb48482bc49c747ffb541c5b2709e831
Eugene Kubesh <e.kubesh@wis.eu.com>
2019-11-26 11:15:26 +0300

    added color option for dialog buttons

--- playground/app/components/modal-example/modal-example.component.ts
+++ playground/app/components/modal-example/modal-example.component.ts
@@ -33,6 +33,7 @@ export class ModalExampleComponent {
       buttons: [
         {
           label: 'Custom Button',
+          color: 'primary',
           click: () => {
             alert('Clicked');
           }
--- src/app/interfaces/message-dialog-config.ts
+++ src/app/interfaces/message-dialog-config.ts
@@ -1,7 +1,8 @@
 import { MessageConfig } from './message-config';
+import { ThemePalette } from '@angular/material/core';
 
 export interface MessageDialogConfig extends MessageConfig {
-  buttons?: { label: string, click: Function }[],
+  buttons?: { label: string, click: Function, color?: ThemePalette }[],
   backdropClass?: string,
   panelClass?: string,
   width?: string
