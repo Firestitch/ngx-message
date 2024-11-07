3e896d63eaab7c07a16f78fdede5a5a589c8db55
Anatoly Thernytskyi <mendor1990@gmail.com>
2024-11-07 13:20:24 +0200

    styling issue

--- playground/app/playground.module.ts
+++ playground/app/playground.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
 
 import { FsExampleModule } from '@firestitch/example';
 import { FsMessageModule } from '@firestitch/message';
 
-import { FsMaterialModule } from './material.module';
+import { AppComponent } from './app.component';
 import { BannerExampleComponent } from './components/banner-example/banner-example.component';
 import { BannerInlineExampleComponent } from './components/banner-inline-example/banner-inline-example.component';
 import { ModalExampleComponent } from './components/modal-example/modal-example.component';
 import { ToastExampleComponent } from './components/toast-example/toast-example.component';
+import { FsMaterialModule } from './material.module';
 
 
 @NgModule({
-  bootstrap: [ AppComponent ],
+  bootstrap: [AppComponent],
   imports: [
+    RouterModule.forRoot([]),
     BrowserModule,
     FsMessageModule.forRoot({
       toastTimeout: 3,
